1d46a5b5ec31a1c08b81f3cf88376efc300cad8f
yuuuuuuan <1395131175@qq.com>
2023-10-16 19:23:42 +0800

    Initial commit

---
+++ game.go
@@ -0,0 +1,37 @@
+package game
+
+import "github.com/hajimehoshi/ebiten/v2"
+
+const (
+	screenWidth  = 600
+	screenHeight = 500
+)
+
+type Position struct {
+	x      int
+	y      int
+	width  int
+	height int
+	img    *ebiten.Image
+}
+
+type Game struct {
+	input *Input
+}
+
+func (g *Game) Update() error {
+	g.input.Update()
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+
+}
+
+func (g *Game) Layout(int, int) (int, int) {
+	return screenWidth, screenHeight
+}
+
+func NewGame() *Game {
+	return &Game{}
+}
---
+++ man.go
@@ -0,0 +1,10 @@
+package game
+
+type Man struct {
+	p           Position
+	frameOX     int
+	frameOY     int
+	frameWidth  int
+	frameHeight int
+	frameCount  int
+}
